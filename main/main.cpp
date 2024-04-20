c0aa63f47e8aab8d01561659f2e10ce3c47ea059
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-20 23:49:23 +0300

    add doxygen comments

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,28 @@
 #include <cstring>
 #include <iostream>
 
+/**
+ * @brief Проверяет свойство текста: текст начинается заглавной латинской буквой
+ * и оканчивается заглавной латинской буквой
+ * @param str: входной текст
+ * @return true
+ * @return false
+ */
 extern "C" __attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital(
     char const* str);
+
+/**
+ * @brief Производит операцию над текстом: заменить каждую латинскую букву
+ * символом '*'
+ * @param str: входной текст
+ */
 extern "C" __attribute__((sysv_abi)) void ChangeAllLatinToStar(char* str);
+
+/**
+ * @brief Производит операцию над текстом: удалить из текста все повторные
+ * вхождения его первого символа
+ * @param str: входной текст
+ */
 extern "C" __attribute__((sysv_abi)) void RemoveAllFirstRepeats(char* str);
 
 extern "C" __attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital_C(

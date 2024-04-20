f196fdaa6724fdcf52a989a197957a99e8549880
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-20 23:39:31 +0300

    add C functions declaration and sysv_abi attribute

---
+++ main/funcs.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+__attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital_C(char const* str) {}
+
+__attribute__((sysv_abi)) void ChangeAllLatinToStar_C(char* str) {}
+
+__attribute__((sysv_abi)) void RemoveAllFirstRepeats_C(char* str) {}
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,14 @@
 #include <cstring>
 #include <iostream>
 
-extern "C" bool IsStartsAndEndsWithCapital(char const* str);
-extern "C" void ChangeAllLatinToStar(char* str);
-extern "C" void RemoveAllFirstRepeats(char* str);
+extern "C" __attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital(
+    char const* str);
+extern "C" __attribute__((sysv_abi)) void ChangeAllLatinToStar(char* str);
+extern "C" __attribute__((sysv_abi)) void RemoveAllFirstRepeats(char* str);
+
+extern "C" __attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital_C(
+    char const* str);
+extern "C" __attribute__((sysv_abi)) void ChangeAllLatinToStar_C(char* str);
+extern "C" __attribute__((sysv_abi)) void RemoveAllFirstRepeats_C(char* str);
 
 int main() { return 0; }

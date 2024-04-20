04537d712147a27b8cc159ba6adad30f46cc7ad7
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-21 00:51:06 +0300

    add realization of first function (IsStartsAndEndsWithCapital_C)

--- main/funcs.c
+++ main/funcs.c
@@ -1,6 +1,33 @@
+#include <assert.h>
+#include <ctype.h>
 #include <stdbool.h>
 
-__attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital_C(char const* str) {}
+// ---------------------- helping static functions ----------------------
+
+__attribute__((sysv_abi)) static char LastOf(const char* str) {
+  assert(str);
+
+  char c = '\0';
+  for (; *str != '\0'; ++str) c = *str;
+
+  return c;
+}
+
+__attribute__((sysv_abi)) static char FirstOf(const char* str) {
+  assert(str);
+  return str[0];
+}
+
+// ----------------------     task functions     ----------------------
+
+__attribute__((sysv_abi)) bool IsStartsAndEndsWithCapital_C(char const* str) {
+  assert(str);
+
+  char f = FirstOf(str);
+  char l = LastOf(str);
+
+  return isupper(f) == isupper(l);
+}
 
 __attribute__((sysv_abi)) void ChangeAllLatinToStar_C(char* str) {}
 

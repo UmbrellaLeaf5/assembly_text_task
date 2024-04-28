f9369d8caaafc1092e404f0e5e0c0a063e219a2c
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-28 20:26:11 +0300

    complete tests for extra helping funcs

--- tests/assembly_funcs/extra_help.cpp
+++ tests/assembly_funcs/extra_help.cpp
@@ -19,4 +19,47 @@ BOOST_AUTO_TEST_CASE(Simple_test) {
   BOOST_TEST(LastOf("X") == 'X');
 }
 
+BOOST_AUTO_TEST_CASE(EmptyStr_test) {
+  BOOST_TEST(FirstOf("") == '\0');
+  BOOST_TEST(FirstOf(" ") == ' ');
+  BOOST_TEST(FirstOf(nullptr) == '\0');
+
+  BOOST_TEST(LastOf("") == '\0');
+  BOOST_TEST(LastOf(" ") == ' ');
+  BOOST_TEST(LastOf(nullptr) == '\0');
+}
+
+BOOST_AUTO_TEST_CASE(Long_test) {
+  std::string long_string(1000, 'X');
+
+  BOOST_TEST(FirstOf(long_string.c_str()) == 'X');
+  BOOST_TEST(LastOf(long_string.c_str()) == 'X');
+
+  // вставка спец. символов в начало строки
+  long_string.insert(0, 1, '\t');
+  long_string.insert(1, 1, '\n');
+  long_string.insert(2, 1, '\r');
+
+  BOOST_TEST(FirstOf(long_string.c_str()) == '\t');
+
+  // вставка спец. символов в конец строки
+  long_string.append(1, '\t');
+  long_string.append(1, '\n');
+  long_string.append(1, '\r');
+
+  BOOST_TEST(LastOf(long_string.c_str()) == '\r');
+}
+
+BOOST_AUTO_TEST_CASE(Special_test) {
+  BOOST_TEST(FirstOf("\n\t\r") == '\n');
+  BOOST_TEST(FirstOf("\0ABC") == '\0');
+  BOOST_TEST(FirstOf("\t\n\r") == '\t');
+  BOOST_TEST(FirstOf("\x01\x02\x03") == '\x01');
+
+  BOOST_TEST(LastOf("\n\t\r") == '\r');
+  BOOST_TEST(LastOf("ABC\0") == 'C');
+  BOOST_TEST(LastOf("\t\n\r") == '\r');
+  BOOST_TEST(LastOf("\x01\x02\x03") == '\x03');
+}
+
 BOOST_AUTO_TEST_SUITE_END()

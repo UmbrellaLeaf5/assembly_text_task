bbb3f3c8e936b9aa8b445f893210d132cd8d2423
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-28 18:31:34 +0300

    add LastOf sub-func realization

--- assembly_funcs/is_starts_and_ends_with_capital.s
+++ assembly_funcs/is_starts_and_ends_with_capital.s
@@ -1,3 +1,15 @@
   .globl	IsStartsAndEndsWithCapital
 IsStartsAndEndsWithCapital:
   ret
+
+LastOf:
+  xorb %al, %al
+.L1:
+  movb (%rdi), %r8b
+  testb %r8b, %r8b
+  je .L2
+  movb %r8b, %al 
+  incq %rdi
+  jmp .L1
+.L2:
+  ret

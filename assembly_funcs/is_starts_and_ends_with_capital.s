279389aa6ece8cfd41908024131f3606f45d6294
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-28 19:07:31 +0300

    add FirstOf realization and modify LastOf

--- assembly_funcs/is_starts_and_ends_with_capital.s
+++ assembly_funcs/is_starts_and_ends_with_capital.s
@@ -2,14 +2,22 @@
 IsStartsAndEndsWithCapital:
   ret
 
+FirstOf:
+  movb (%rdi), %r8b   # get a byte
+  testb %r8b, %r8b    # *s ?= '\0'
+  jne	.FONotEqual     # if (*s != '\0') c = *s; 
+.FONotEqual:
+  movb %r8b, %al
+  ret
+
 LastOf:
-  xorb %al, %al
-.L1:
-  movb (%rdi), %r8b
-  testb %r8b, %r8b
-  je .L2
-  movb %r8b, %al 
-  incq %rdi
-  jmp .L1
-.L2:
+  xorb %al, %al       # init: c = 0
+.LOIter:
+  movb (%rdi), %r8b   # get a byte
+  testb %r8b, %r8b    # *s ?= '\0'
+  je .LORet           # if (*s == '\0') return; 
+  movb %r8b, %al      # else c = *s;
+  incq %rdi           # next byte
+  jmp .LOIter         # continue iteration
+.LORet:
   ret

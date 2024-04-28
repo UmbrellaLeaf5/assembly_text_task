845e70f61ff668729719e0e734da38c118de7850
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-28 14:56:14 +0300

    add primary test for first C-function

--- tests/is_starts_and_ends_with_capital_test.cpp
+++ tests/is_starts_and_ends_with_capital_test.cpp
@@ -6,16 +6,15 @@
 namespace tt = boost::test_tools;
 namespace utf = boost::unit_test;
 
+extern "C" bool IsStartsAndEndsWithCapital_C(char const* str);
+
 BOOST_AUTO_TEST_SUITE(is_starts_and_ends_with_capital_test)
 
-BOOST_AUTO_TEST_CASE(simple) {
-  struct {
-    double x, y;
-  } cases[] = {{0, 0}, {1, 1}, {2, 2}};
-
-  for (auto c : cases) {
-    BOOST_TEST(c.y == c.x);
-  }
+BOOST_AUTO_TEST_CASE(hello_test) {
+  BOOST_TEST(IsStartsAndEndsWithCapital_C("Hello") == false);
+  BOOST_TEST(IsStartsAndEndsWithCapital_C("hello") == false);
+  BOOST_TEST(IsStartsAndEndsWithCapital_C("H") == true);
+  BOOST_TEST(IsStartsAndEndsWithCapital_C("") == false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

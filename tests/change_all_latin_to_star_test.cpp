8f2f4c259ff3fbb911d9958d33e1386ac7368062
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-28 16:34:31 +0300

    add const to char expected[]

--- tests/change_all_latin_to_star_test.cpp
+++ tests/change_all_latin_to_star_test.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(ChangeAllLatinToStar_C_Tests)
 
 BOOST_AUTO_TEST_CASE(Simple_test) {
   char str[] = "HelloWorld";
-  char expected[] = "**********";
+  const char expected[] = "**********";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 }
@@ -25,77 +25,77 @@ BOOST_AUTO_TEST_CASE(EmptyStr_test) {
 
 BOOST_AUTO_TEST_CASE(Extra_test) {
   char str[] = "Hello123!@#$";
-  char expected[] = "*****123!@#$";
+  const char expected[] = "*****123!@#$";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 
   char str_2[] = "Hello123World456";
-  char expected_2[] = "*****123*****456";
+  const char expected_2[] = "*****123*****456";
   ChangeAllLatinToStar_C(str_2);
   BOOST_TEST(std::strcmp(str_2, expected_2) == 0);
 
   char str_3[] = "Hello!@#$%^&*()_+World";
-  char expected_3[] = "*****!@#$%^&*()_+*****";
+  const char expected_3[] = "*****!@#$%^&*()_+*****";
   ChangeAllLatinToStar_C(str_3);
   BOOST_TEST(std::strcmp(str_3, expected_3) == 0);
 
   char str_4[] = "123!@#$%^&*()";
-  char expected_4[] = "123!@#$%^&*()";
+  const char expected_4[] = "123!@#$%^&*()";
   ChangeAllLatinToStar_C(str_4);
   BOOST_TEST(std::strcmp(str_4, expected_4) == 0);
 
   char str_5[] = "The price is $19.99 or €17.50";
-  char expected_5[] = "*** ***** ** $19.99 ** €17.50";
+  const char expected_5[] = "*** ***** ** $19.99 ** €17.50";
   ChangeAllLatinToStar_C(str_5);
   BOOST_TEST(std::strcmp(str_5, expected_5) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(Long_test) {
   char str[] = "ThisIsAVeryLongStringWithLotsOfLatinCharacters";
-  char expected[] = "**********************************************";
+  const char expected[] = "**********************************************";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(Repeat_test) {
   char str[] = "aaaabbbcccdddeeefff";
-  char expected[] = "*******************";
+  const char expected[] = "*******************";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(Special_test) {
   char str[] = "Hello\nWorld\tTest";
-  char expected[] = "*****\n*****\t****";
+  const char expected[] = "*****\n*****\t****";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 
   char str_2[] = "Hello\nWorld";
-  char expected_2[] = "*****\n*****";
+  const char expected_2[] = "*****\n*****";
   ChangeAllLatinToStar_C(str_2);
   BOOST_TEST(std::strcmp(str_2, expected_2) == 0);
 
   char str_3[] = "Hello\tWorld";
-  char expected_3[] = "*****\t*****";
+  const char expected_3[] = "*****\t*****";
   ChangeAllLatinToStar_C(str_3);
   BOOST_TEST(std::strcmp(str_3, expected_3) == 0);
 
   char str_4[] = "Hello\nWorld\tTest";
-  char expected_4[] = "*****\n*****\t****";
+  const char expected_4[] = "*****\n*****\t****";
   ChangeAllLatinToStar_C(str_4);
   BOOST_TEST(std::strcmp(str_4, expected_4) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(French_test) {
   char str[] = "Bonjour, Hélène!";
-  char expected[] = "*******, *é*è**!";
+  const char expected[] = "*******, *é*è**!";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(Math_test) {
   char str[] = "x^2 + y^2 = z^2";
-  char expected[] = "*^2 + *^2 = *^2";
+  const char expected[] = "*^2 + *^2 = *^2";
   ChangeAllLatinToStar_C(str);
   BOOST_TEST(std::strcmp(str, expected) == 0);
 }

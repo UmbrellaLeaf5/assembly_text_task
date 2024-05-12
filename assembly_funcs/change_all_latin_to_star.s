95bafd0f0289aa94112f15caf97655dfca950ce2
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-05-12 16:42:33 +0300

    add realization of second assembly-func

--- assembly_funcs/change_all_latin_to_star.s
+++ assembly_funcs/change_all_latin_to_star.s
@@ -1,3 +1,26 @@
   .globl	ChangeAllLatinToStar
 ChangeAllLatinToStar:
+  testq %rdi, %rdi    # s ?= nullptr
+  je .CALTSRet        # if (s == nullptr) return; 
+
+.CALTSIter:
+  movb (%rdi), %al    # get a byte (char)
+  testb %al, %al      # *s ?= '\0'
+  je .CALTSRet        # if (*s == '\0') return;
+
+  pushq %rax          # push char on stack
+  movsbl %al, %ecx    # char -> int
+  call isalpha
+  popq %rsi           # pop char from stack
+
+  testl %eax, %eax    # ? !isalpha(char)
+  je .CALTSToNext     # if (!isalpha(c)) continue;
+
+  movb $42, (%rdi)     # else: *char = '*';
+
+.CALTSToNext:
+  incq %rdi           # next byte (char)
+  jmp .CALTSIter      # continue iteration
+
+.CALTSRet:
   ret

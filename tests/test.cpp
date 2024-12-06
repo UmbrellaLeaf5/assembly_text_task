8df31b7fb270c5bf74bc963333127eddb49c82e1
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-12-06 15:48:00 +0300

    fix warnings

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MAIN
-#if !defined(WIN32)
+
+#if !defined(WIN32) && !defined(BOOST_TEST_DYN_LINK)
 #define BOOST_TEST_DYN_LINK
 #endif
+
 #define BOOST_TEST_MODULE AllTests
 #include <boost/test/unit_test.hpp>
